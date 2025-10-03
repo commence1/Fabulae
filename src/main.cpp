883458a3c4d176ef6dcccc3586023ff7015a40aa
commence1 <2364045784@qq.com>
2025-10-03 12:32:19 +0800

    First Commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,21 @@
-int main() {
-    return 0;
+#include <Vulkan/vulkan.hpp>
+#include <SDL3/sdl.h>
+#include <SDL3/sdl_main.h>
+
+#include <decode.h>
+
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
+    return SDL_APP_CONTINUE;
+}
+SDL_AppResult SDL_AppEvent(void *appstate, const SDL_Event *event) {
+    return SDL_APP_CONTINUE;
+}
+SDL_AppResult SDL_AppIterate(void *appstate) {
+    return SDL_APP_CONTINUE;
+}
+SDL_AppResult SDL_AppRender(void *appstate) {
+    return SDL_APP_CONTINUE;
+}
+void SDL_Quit(void *appstate) {
+    SDL_Quit();
 }
