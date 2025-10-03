0fb81bbe0c71d681815f4a855fe8d614e1fc6585
commence1 <2364045784@qq.com>
2025-10-03 12:46:38 +0800

    First Commit

---
+++ Include/resources.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <SDL3/SDL.h>
+
+class Resources {
+public:
+    static SDL_Window* window;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
-#include <Vulkan/vulkan.hpp>
-#include <SDL3/sdl.h>
-#include <SDL3/sdl_main.h>
+#include <vulkan/vulkan.hpp>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
 #include <decode.h>
+#include <resources.h>
+
+#define WIDTH 800
+#define HEIGHT 600
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
+    Resources::window = SDL_CreateWindow("Fabulae", WIDTH, HEIGHT, SDL_WINDOW_VULKAN | SDL_WINDOW_RESIZABLE);
+    if (!Resources::window) {
+        SDL_Log("Failed to create window: %s", SDL_GetError());
+    }
     return SDL_APP_CONTINUE;
 }
 SDL_AppResult SDL_AppEvent(void *appstate, const SDL_Event *event) {
